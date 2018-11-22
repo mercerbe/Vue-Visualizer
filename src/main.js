50027a83c3ad66b2aef137beb8dfe977dd72d664
Benjamin Mercer <mercerbe@gmail.com>
2018-11-22 18:04:38 -0500

    add pattern

--- src/main.js
+++ src/main.js
@@ -5,20 +5,20 @@ Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
-  el:"#root",
-  data: {styleOne: {}, styleTwo: {}},
+  el:"#app",
+  data: { styleOne: {}, styleTwo: {} },
   methods: {
-    onMouseMove(e) {
-      this.styleOne = transform(-e.clientX / e.clientY)
-      this.styleTwo = transform(e.clientX /e.clientY)
+    onMouseMove(event) {
+      this.styleOne = transform(-event.clientX / event.clientY);
+      this.styleTwo = transform(event.clientX / event.clientY);
     }
   }
 }).$mount('#app')
 
 function transform(offset) {
-  const cos = Matn.cos(offset);
+  const cos = Math.cos(offset);
   const sin = Math.sin(offset);
   return {
     transform: `matrix3d(${sin}, ${-cos}, ${sin}, 0, ${-cos}, ${sin}, 0, 0, 0, ${cos}, ${cos}, ${sin}, 0, 0, 0, 1)`
-  }
+  };
 }
